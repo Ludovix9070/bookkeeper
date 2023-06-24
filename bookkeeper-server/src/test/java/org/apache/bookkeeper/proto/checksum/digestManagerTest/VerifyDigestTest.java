ce344dfd39e1b7a2e5a5d27dcab5627aaa7f9112
ludovico <ludovix9070@gmail.com>
2023-06-24 14:36:17 +0200

    Updated VerifyDigestTest, 100% coverage

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/proto/checksum/digestManagerTest/VerifyDigestTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/proto/checksum/digestManagerTest/VerifyDigestTest.java
@@ -21,8 +21,7 @@ import java.util.Random;
 @RunWith(value = Parameterized.class)
 public class VerifyDigestTest extends TestCase{
         private static DigestManager digestManager;
-
-        private static long lastAddConfirmed;
+        private static DigestManager digestManager32;
         private static boolean flagIsV2Proto = false;
         private static final long length = 1024;
         private ByteBuf dataReceived;
@@ -31,21 +30,23 @@ public class VerifyDigestTest extends TestCase{
         private static final long ledgerId = 1;
 
         private boolean skipEntryIdCheck;
+        private boolean need32Manager;
         private ByteBuf entryTest;
         private static int dummyInt = 1;
         private static byte[] dummy = null;
         private static String bufferString = generateRandomString(length);
 
 
-        public VerifyDigestTest(long entryId, ByteBuf dataReceived, boolean skipEntryIdCheck, Object expected) {
-            configure(entryId, dataReceived, skipEntryIdCheck, expected);
+        public VerifyDigestTest(long entryId, ByteBuf dataReceived, boolean skipEntryIdCheck, boolean need32Manager, Object expected) {
+            configure(entryId, dataReceived, skipEntryIdCheck, need32Manager, expected);
         }
 
-        private void configure(long entryId, ByteBuf dataReceived, boolean skipEntryIdCheck, Object expected){
+        private void configure(long entryId, ByteBuf dataReceived, boolean skipEntryIdCheck, boolean need32Manager, Object expected){
             this.dataReceived = dataReceived;
             this.entryId = entryId;
             this.expected = expected;
             this.skipEntryIdCheck = skipEntryIdCheck;
+            this.need32Manager = need32Manager;
 
         }
 
@@ -59,31 +60,43 @@ public class VerifyDigestTest extends TestCase{
             return Arrays.asList(new Object[][] {
 
 
-                    //entryId dataReceived skip
-                    {-1,null, false, NullPointerException.class},
-                    {-1,ByteBufList.coalesce(createEntryWithDigest(length,ledgerId,0, DataFormats.LedgerMetadataFormat.DigestType.HMAC)),false, BKException.BKDigestMatchException.class},
-                    {-1,ByteBufList.coalesce(createEntryWithDigest(length,ledgerId,-1, DataFormats.LedgerMetadataFormat.DigestType.HMAC)),false, BKException.BKDigestMatchException.class},
-                    {0,null,false, NullPointerException.class},
-                    {0,ByteBufList.coalesce(createEntryWithDigest(length,ledgerId,1, DataFormats.LedgerMetadataFormat.DigestType.HMAC)),false, BKException.BKDigestMatchException.class},
-                    {0,ByteBufList.coalesce(createEntryWithDigest(length,ledgerId,0, DataFormats.LedgerMetadataFormat.DigestType.HMAC)),false, null},
-                    {1,null,false, NullPointerException.class},
-                    {1,ByteBufList.coalesce(createEntryWithDigest(length,ledgerId,2, DataFormats.LedgerMetadataFormat.DigestType.HMAC)),false, BKException.BKDigestMatchException.class},
-                    {1,ByteBufList.coalesce(createEntryWithDigest(length,ledgerId,1, DataFormats.LedgerMetadataFormat.DigestType.HMAC)),false, null},
+                    //entryId dataReceived skip need32 multidimensional base analysis
+                    {-1,null, false,false, NullPointerException.class},
+                    {-1,ByteBufList.coalesce(createEntryWithDigest(length,ledgerId,0, DataFormats.LedgerMetadataFormat.DigestType.HMAC)),false,false, BKException.BKDigestMatchException.class},
+                    {-1,ByteBufList.coalesce(createEntryWithDigest(length,ledgerId,-1, DataFormats.LedgerMetadataFormat.DigestType.HMAC)),false, false, BKException.BKDigestMatchException.class},
+                    {0,null,false,false, NullPointerException.class},
+                    {0,ByteBufList.coalesce(createEntryWithDigest(length,ledgerId,1, DataFormats.LedgerMetadataFormat.DigestType.HMAC)),false, false, BKException.BKDigestMatchException.class},
+                    {0,ByteBufList.coalesce(createEntryWithDigest(length,ledgerId,0, DataFormats.LedgerMetadataFormat.DigestType.HMAC)),false,false, null},
+                    {1,null,false,false, NullPointerException.class},
+                    {1,ByteBufList.coalesce(createEntryWithDigest(length,ledgerId,2, DataFormats.LedgerMetadataFormat.DigestType.HMAC)),false,false, BKException.BKDigestMatchException.class},
+                    {1,ByteBufList.coalesce(createEntryWithDigest(length,ledgerId,1, DataFormats.LedgerMetadataFormat.DigestType.HMAC)),false,false, null},
 
-
+                    //tests to have a better coverage
+                    {1,ByteBufList.coalesce(createBadShortEntryWithDigest()),false,false, BKException.BKDigestMatchException.class},
+                    {1,ByteBufList.coalesce(createEntryWithDigest(length,ledgerId,1, DataFormats.LedgerMetadataFormat.DigestType.CRC32)),false,false, BKException.BKDigestMatchException.class},
+                    {1,ByteBufList.coalesce(createEntryWithDigest(length,ledgerId,1, DataFormats.LedgerMetadataFormat.DigestType.HMAC)),false,true, BKException.BKDigestMatchException.class},
+                    {1,ByteBufList.coalesce(createEntryWithDigest(length,ledgerId+1,1, DataFormats.LedgerMetadataFormat.DigestType.HMAC)),false,false, BKException.BKDigestMatchException.class},
+                    {1,ByteBufList.coalesce(createEntryWithDigest(length,ledgerId,1, DataFormats.LedgerMetadataFormat.DigestType.CRC32C)),false,true, null},
             });
         }
 
         @Before
         public void setUp() throws GeneralSecurityException {
             this.digestManager = DigestManager.instantiate(ledgerId, "password".getBytes(), DataFormats.LedgerMetadataFormat.DigestType.HMAC, UnpooledByteBufAllocator.DEFAULT, flagIsV2Proto);
+            this.digestManager32 = DigestManager.instantiate(ledgerId, "password".getBytes(), DataFormats.LedgerMetadataFormat.DigestType.CRC32C, UnpooledByteBufAllocator.DEFAULT, flagIsV2Proto);
             this.entryTest = createEntry(length);
         }
 
         @Test
         public void testVerifyDigest() {
             try {
-                ByteBuf dataBack = digestManager.verifyDigestAndReturnData(entryId, dataReceived);
+
+                ByteBuf dataBack;
+                if(need32Manager){
+                    dataBack = digestManager32.verifyDigestAndReturnData(entryId, dataReceived);
+                }else{
+                    dataBack = digestManager.verifyDigestAndReturnData(entryId, dataReceived);
+                }
                 ByteBuf dataToCompare = Unpooled.buffer((int)length);
                 dataToCompare.writeBytes(dataBack,0,(int)length);
                 assertEquals(this.entryTest.toString(), dataToCompare.toString());
@@ -99,6 +112,16 @@ public class VerifyDigestTest extends TestCase{
         ByteBufList byteBufList = (ByteBufList) digestMan.computeDigestAndPackageForSending(inputEntryId, 0,  length, byteBuffer, dummy, dummyInt);
         return byteBufList;
     }
+
+    private static ByteBufList createBadShortEntryWithDigest() throws GeneralSecurityException {
+        int shorterLength = 20; //must be less than 52
+        byte[] data = generateRandomString(shorterLength).getBytes();
+        ByteBuf byteBuffer = Unpooled.buffer(shorterLength);
+        byteBuffer.writeBytes(data);
+        return ByteBufList.get(byteBuffer);
+    }
+
+
 
     private static ByteBuf createEntry(long length) {
         byte[] data = bufferString.getBytes();
